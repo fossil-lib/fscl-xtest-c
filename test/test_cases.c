09192a4d14a7e593c99de3b6edcdf78ab6931a36
Michael Gene Brockus <michaelbrockus@gmail.com>
2023-06-26 18:06:11 -0600

    update test to work

--- test/test_cases.c
+++ test/test_cases.c
@@ -56,51 +56,23 @@ static void test_02_assertInts()
     tril_xtest_assert(first == tril_xmock_stub_medium_int(), "first is 50 should assert equal");
 } // end of test case
 
-static void test_03_assertFloats()
+static void test_03_assertStrings()
 {
     //
     // we setup are test data here.
-    const xmock_float left = 22.0f, right = 24.0f;
+    const xmock_string first = "Something", second = "Something else";
 
     //
     // here we run the asserts
-    tril_xtest_assert(!(fabs(left - 22.0) <= __FLT_EPSILON__), "assert float should be 22.0");
-    tril_xtest_assert(fabs(left - right) <= __FLT_EPSILON__, "assert left and right are not equal");
-    tril_xtest_assert(left > right - __FLT_EPSILON__, "assert left is less than right");
-    tril_xtest_assert(right < left + __FLT_EPSILON__, "assert left is more than right");
-} // end of test case
-
-static void test_04_assertDouble()
-{
-    //
-    // we setup are test data here.
-    const xmock_double left = 22.0f, right = 24.0f;
-
-    //
-    // here we run the asserts
-    tril_xtest_assert(!(fabs(left - 22.0) <= __DBL_EPSILON__), "assert float should be 22.0");
-    tril_xtest_assert(fabs(left - right) <= __DBL_EPSILON__, "assert left and right are not equal");
-    tril_xtest_assert(left > right - __DBL_EPSILON__, "assert left is less than right");
-    tril_xtest_assert(right < left + __DBL_EPSILON__, "assert left is more than right");
-} // end of test case
-
-static void test_05_assertStrings()
-{
-    //
-    // we setup are test data here.
-    const xmock_string first = "Something", second = "Some other word";
-
-    //
-    // here we run the asserts
-    tril_xtest_assert(!(strcmp(first, "Something") == 0), "first word is not equal to second word");
-    tril_xtest_assert(strcmp(first, second) == 0, "first word is equal to second word");
+    tril_xtest_assert(strcmp(first, "Something") == 0, "first word is not equal to second word");
+    tril_xtest_assert(!(strcmp(first, second) == 0), "first word is equal to second word");
     tril_xtest_assert(strlen(first) == 9, "first word should be 9 chars in length");
     tril_xtest_assert(strlen(second) != 9, "second word should not be 9 chars in length");
     tril_xtest_assert(strcmp(first, second) < 0, "first word is less than second word");
     tril_xtest_assert(strcmp(second, first) > 0, "second word is more than first word");
 } // end of test case
 
-static void test_06_assertPointer()
+static void test_04_assertPointer()
 {
     //
     // we setup are test data here.
@@ -112,7 +84,7 @@ static void test_06_assertPointer()
     tril_xtest_assert(other == tril_xmock_stub_nullptr(), "should assert to null value");
 } // end of test case
 
-static void test_07_expectBools()
+static void test_05_expectBools()
 {
     //
     // we setup are test data here.
@@ -124,7 +96,7 @@ static void test_07_expectBools()
     tril_xtest_expect(falseValue == xmock_false, "should assert to false");
 } // end of test case
 
-static void test_08_expectInts()
+static void test_06_expectInts()
 {
     //
     // we setup are test data here.
@@ -136,51 +108,23 @@ static void test_08_expectInts()
     tril_xtest_expect(first == tril_xmock_stub_medium_int(), "first is 50 should assert equal");
 } // end of test case
 
-static void test_09_expectFloats()
+static void test_07_expectStrings()
 {
     //
     // we setup are test data here.
-    const xmock_float left = 22.0f, right = 24.0f;
+    const xmock_string first = "Something", second = "Something else";
 
     //
     // here we run the asserts
-    tril_xtest_expect(!(fabs(left - 22.0) <= __FLT_EPSILON__), "assert float should be 22.0");
-    tril_xtest_expect(fabs(left - right) <= __FLT_EPSILON__, "assert left and right are not equal");
-    tril_xtest_expect(left > right - __FLT_EPSILON__, "assert left is less than right");
-    tril_xtest_expect(right < left + __FLT_EPSILON__, "assert left is more than right");
-} // end of test case
-
-static void test_10_expectDouble()
-{
-    //
-    // we setup are test data here.
-    const xmock_double left = 22.0f, right = 24.0f;
-
-    //
-    // here we run the asserts
-    tril_xtest_expect(!(fabs(left - 22.0) <= __DBL_EPSILON__), "assert float should be 22.0");
-    tril_xtest_expect(fabs(left - right) <= __DBL_EPSILON__, "assert left and right are not equal");
-    tril_xtest_expect(left > right - __DBL_EPSILON__, "assert left is less than right");
-    tril_xtest_expect(right < left + __DBL_EPSILON__, "assert left is more than right");
-} // end of test case
-
-static void test_11_expectStrings()
-{
-    //
-    // we setup are test data here.
-    const xmock_string first = "Something", second = "Some other word";
-
-    //
-    // here we run the asserts
-    tril_xtest_expect(!(strcmp(first, "Something") == 0), "first word is not equal to second word");
-    tril_xtest_expect(strcmp(first, second) == 0, "first word is equal to second word");
+    tril_xtest_expect(strcmp(first, "Something") == 0, "first word is not equal to second word");
+    tril_xtest_expect(!(strcmp(first, second) == 0), "first word is equal to second word");
     tril_xtest_expect(strlen(first) == 9, "first word should be 9 chars in length");
     tril_xtest_expect(strlen(second) != 9, "second word should not be 9 chars in length");
     tril_xtest_expect(strcmp(first, second) < 0, "first word is less than second word");
     tril_xtest_expect(strcmp(second, first) > 0, "second word is more than first word");
 } // end of test case
 
-static void test_12_expectPointer()
+static void test_08_expectPointer()
 {
     //
     // we setup are test data here.
@@ -206,23 +150,12 @@ int simpleArraySum(int count, int* ar)
     return sum;
 } // end of func
 
-
-static void test_13_benchArraySum()
+static void test_09_runAssert()
 {
     //
-    // we setup are test data here.
-    xmock_int32 array[] = {1, 2, 3, 4, 10, 11};
-    xmock_int32 sum = 0;
-
-    //
     // here we run the asserts
-    tril_xbench_start();
-    sum = simpleArraySum(6, array);
-    tril_xbench_end();
-
-    tril_xtest_assert(sum == 31, "assert of array sum should be 31");
+    tril_xtest_assert(xmock_true == 0, "this simple assert should run");
 } // end of test case
-
 
 
 /*
@@ -244,17 +177,13 @@ int main(void)
     // project.
     tril_xtest_run(runner, test_01_assertBools);
     tril_xtest_run(runner, test_02_assertInts);
-    tril_xtest_run(runner, test_03_assertFloats);
-    tril_xtest_run(runner, test_04_assertDouble);
-    tril_xtest_run(runner, test_05_assertStrings);
-    tril_xtest_run(runner, test_06_assertPointer);
-    tril_xtest_run(runner, test_07_expectBools);
-    tril_xtest_run(runner, test_08_expectInts);
-    tril_xtest_run(runner, test_09_expectFloats);
-    tril_xtest_run(runner, test_10_expectDouble);
-    tril_xtest_run(runner, test_11_expectStrings);
-    tril_xtest_run(runner, test_12_expectPointer);
-    tril_xtest_run(runner, test_13_benchArraySum);
+    tril_xtest_run(runner, test_03_assertStrings);
+    tril_xtest_run(runner, test_04_assertPointer);
+    tril_xtest_run(runner, test_05_expectBools);
+    tril_xtest_run(runner, test_06_expectInts);
+    tril_xtest_run(runner, test_07_expectStrings);
+    tril_xtest_run(runner, test_08_expectPointer);
+    tril_xtest_run(runner, test_09_runAssert);
 
     return tril_xtest_end_runner(runner);
 } // end of function main
