161b5effcff705488313b76e12461698b5aa7774
Michael Gene Brockus <michaelbrockus@gmail.com>
2023-06-26 18:05:53 -0600

    move struct into source

--- include/trilobite/xtest.h
+++ include/trilobite/xtest.h
@@ -16,39 +16,6 @@ extern "C"
 #include "xmock.h"
 #include "xassert.h"
 
-/*
-  overview:
-  > As the name suggests, UTestRunner is a tool
-  > that is used to run or execute tests and track
-  > results. The runner has a setup and teardown
-  > function built-in and are set to nullptr by
-  > default, the structure also has a range of
-  > score values for pass/fail and more.
-  >
-  args:
-  -> _pass: pass test score value
-  -> _fail: fail test score value
-  -> _skip: skip test score value
-  -> _error: error test score value
-  -> _total: total test score run
-  -> _setup: Test case setup function
-  -> _teardown: Test case teardown function
-*/
-typedef struct UTestRunner
-{
-    // pass/fail counters
-    int _pass;
-    int _fail;
-    int _error;
-    int _skip;
-    int _total;
-
-    // setup/teardown functions
-    void (*_setup)();
-    void (*_teardown)();
-
-} UTestRunner; // end struct
-
 //
 //   PUBLIC APPLICATION INTERFACE
 // --------------------------------
@@ -56,7 +23,10 @@ typedef struct UTestRunner
 // Published library with provided public application interface for
 // use in the users application. Please note that we are internationally
 // targeting both c11 and c23 standards.
-//   
+//
+
+typedef struct UTestRunner UTestRunner; // end struct
+
 UTestRunner *tril_xtest_create_runner(void);
 int tril_xtest_end_runner(UTestRunner *runner);
 void tril_xtest_setup(UTestRunner *runner, void (*func)());
