3a5b7f891ca17305763cc24dfc20c26b174754fb
Michael Gene Brockus <michaelbrockus@gmail.com>
2023-08-25 12:34:52 -0600

    finish rewrite of test cases

--- test/xtest_cases.c
+++ test/xtest_cases.c
@@ -65,23 +65,41 @@ XTEST_CASE(basic_run_of_oct) {
 } // end case
 
 XTEST_CASE(basic_run_of_float) {
-    //
+    float this = 40.00, that = 26.00, other = 40.00;
+
+    XASSERT_FLOAT_EQUAL(this, other, that,"'x' should be equal to 'y'");
+    XASSERT_FLOAT_GREATER(this, that, "'x' should be greater than 'y'");
+    XASSERT_FLOAT_GREATER_EQUAL(this, that, "'x' should be greater or equal to 'y'");
+    XASSERT_FLOAT_LESS(that, other, "'x' should be lesser than 'y'");
+    XASSERT_FLOAT_LESS_EQUAL(that, this, "'x' should be lesser or equal to 'y'");
+    XASSERT_FLOAT_NOT_EQUAL(this, that, 50.00, "'x' should not be equal to 'y'");
 } // end case
 
 XTEST_CASE(basic_run_of_double) {
-    //
+    double this = 40.00, that = 26.00, other = 40.00;
+
+    XASSERT_FLOAT_EQUAL(this, other, that,"'x' should be equal to 'y'");
+    XASSERT_FLOAT_GREATER(this, that, "'x' should be greater than 'y'");
+    XASSERT_FLOAT_GREATER_EQUAL(this, that, "'x' should be greater or equal to 'y'");
+    XASSERT_FLOAT_LESS(that, other, "'x' should be lesser than 'y'");
+    XASSERT_FLOAT_LESS_EQUAL(that, this, "'x' should be lesser or equal to 'y'");
+    XASSERT_FLOAT_NOT_EQUAL(this, that, 50.00, "'x' should not be equal to 'y'");
 } // end case
 
 XTEST_CASE(basic_run_of_string) {
-    //
+    char *one = "Something", *two = "Whatever", *three = "Something";
+    XASSERT_STRING_EQUAL(one, three, "one and three should have benn equal");
+    XASSERT_STRING_NOT_EQUAL(one, two, "one and two should not be equal");
 } // end case
 
 XTEST_CASE(basic_run_of_pointer) {
-    //
+    XASSERT_NOT_NULL("Coffee Cup", "as a coffee cup should have been a non null value");
+    XASSERT_NULL(NULL, "Why didn't this cup of tea return as null?");
 } // end case
 
 XTEST_CASE(basic_run_of_boolean) {
-    //
+    XASSERT_TRUE(true, "should have returned true from a true value");
+    XASSERT_FALSE(false, "should have returned false from a false value");
 } // end case
 
 
