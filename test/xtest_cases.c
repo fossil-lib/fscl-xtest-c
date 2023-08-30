7a32d24d9fd45dc8c2718bc0b41e51e816b775b4
Michael Gene Brockus (Dreamer) <55331536+dreamer-coding-555@users.noreply.github.com>
2023-08-29 21:40:43 -0500

    Update xtest_cases.c

--- test/xtest_cases.c
+++ test/xtest_cases.c
@@ -5,6 +5,7 @@
    website: <https://trilobite.code.blog>
 */
 #include "xtest_fixtures.h"
+#include <stdint.h>
 
 //
 // XUNIT TEST CASES
@@ -20,6 +21,50 @@ XTEST_CASE(basic_run_of_int) {
     XASSERT_INT_NOT_EQUAL(this, that,"'x' should not be equal to 'y'");
 } // end case
 
+XTEST_CASE(basic_run_of_int8) {
+    int8_t this = 32, that = 22, other = 32;
+
+    XASSERT_INT8_EQUAL(this, other,"'x' should be equal to 'y'");
+    XASSERT_INT8_GREATER(this, that,"'x' should be greater than 'y'");
+    XASSERT_INT8_GREATER_EQUAL(this, that,"'x' should be greater or equal to 'y'");
+    XASSERT_INT8_LESS(that, other,"'x' should be lesser than 'y'");
+    XASSERT_INT8_LESS_EQUAL(that, this,"'x' should be lesser or equal to 'y'");
+    XASSERT_INT8_NOT_EQUAL(this, that,"'x' should not be equal to 'y'");
+} // end case
+
+XTEST_CASE(basic_run_of_int16) {
+    int16_t this = 32, that = 22, other = 32;
+
+    XASSERT_INT16_EQUAL(this, other,"'x' should be equal to 'y'");
+    XASSERT_INT16_GREATER(this, that,"'x' should be greater than 'y'");
+    XASSERT_INT16_GREATER_EQUAL(this, that,"'x' should be greater or equal to 'y'");
+    XASSERT_INT16_LESS(that, other,"'x' should be lesser than 'y'");
+    XASSERT_INT16_LESS_EQUAL(that, this,"'x' should be lesser or equal to 'y'");
+    XASSERT_INT16_NOT_EQUAL(this, that,"'x' should not be equal to 'y'");
+} // end case
+
+XTEST_CASE(basic_run_of_int32) {
+    int32_t this = 32, that = 22, other = 32;
+
+    XASSERT_INT32_EQUAL(this, other,"'x' should be equal to 'y'");
+    XASSERT_INT32_GREATER(this, that,"'x' should be greater than 'y'");
+    XASSERT_INT32_GREATER_EQUAL(this, that,"'x' should be greater or equal to 'y'");
+    XASSERT_INT32_LESS(that, other,"'x' should be lesser than 'y'");
+    XASSERT_INT32_LESS_EQUAL(that, this,"'x' should be lesser or equal to 'y'");
+    XASSERT_INT32_NOT_EQUAL(this, that,"'x' should not be equal to 'y'");
+} // end case
+
+XTEST_CASE(basic_run_of_int64) {
+    int64_t this = 32, that = 22, other = 32;
+
+    XASSERT_INT64_EQUAL(this, other,"'x' should be equal to 'y'");
+    XASSERT_INT64_GREATER(this, that,"'x' should be greater than 'y'");
+    XASSERT_INT64_GREATER_EQUAL(this, that,"'x' should be greater or equal to 'y'");
+    XASSERT_INT64_LESS(that, other,"'x' should be lesser than 'y'");
+    XASSERT_INT64_LESS_EQUAL(that, this,"'x' should be lesser or equal to 'y'");
+    XASSERT_INT64_NOT_EQUAL(this, that,"'x' should not be equal to 'y'");
+} // end case
+
 XTEST_CASE(basic_run_of_uint) {
     unsigned int this = 32, that = 22, other = 32;
 
@@ -29,6 +74,50 @@ XTEST_CASE(basic_run_of_uint) {
     XASSERT_UINT_LESS(that, other,"'x' should be lesser than 'y'");
     XASSERT_UINT_LESS_EQUAL(that, this,"'x' should be lesser or equal to 'y'");
     XASSERT_UINT_NOT_EQUAL(this, that,"'x' should not be equal to 'y'");
+} // end case
+
+XTEST_CASE(basic_run_of_uint8) {
+    uint8_t this = 32, that = 22, other = 32;
+
+    XASSERT_UINT8_EQUAL(this, other,"'x' should be equal to 'y'");
+    XASSERT_UINT8_GREATER(this, that,"'x' should be greater than 'y'");
+    XASSERT_UINT8_GREATER_EQUAL(this, that,"'x' should be greater or equal to 'y'");
+    XASSERT_UINT8_LESS(that, other,"'x' should be lesser than 'y'");
+    XASSERT_UINT8_LESS_EQUAL(that, this,"'x' should be lesser or equal to 'y'");
+    XASSERT_UINT8_NOT_EQUAL(this, that,"'x' should not be equal to 'y'");
+} // end case
+
+XTEST_CASE(basic_run_of_uint16) {
+    uint16_t this = 32, that = 22, other = 32;
+
+    XASSERT_UINT16_EQUAL(this, other,"'x' should be equal to 'y'");
+    XASSERT_UINT16_GREATER(this, that,"'x' should be greater than 'y'");
+    XASSERT_UINT16_GREATER_EQUAL(this, that,"'x' should be greater or equal to 'y'");
+    XASSERT_UINT16_LESS(that, other,"'x' should be lesser than 'y'");
+    XASSERT_UINT16_LESS_EQUAL(that, this,"'x' should be lesser or equal to 'y'");
+    XASSERT_UINT16_NOT_EQUAL(this, that,"'x' should not be equal to 'y'");
+} // end case
+
+XTEST_CASE(basic_run_of_uint32) {
+    uint32_t this = 32, that = 22, other = 32;
+
+    XASSERT_UINT32_EQUAL(this, other,"'x' should be equal to 'y'");
+    XASSERT_UINT32_GREATER(this, that,"'x' should be greater than 'y'");
+    XASSERT_UINT32_GREATER_EQUAL(this, that,"'x' should be greater or equal to 'y'");
+    XASSERT_UINT32_LESS(that, other,"'x' should be lesser than 'y'");
+    XASSERT_UINT32_LESS_EQUAL(that, this,"'x' should be lesser or equal to 'y'");
+    XASSERT_UINT32_NOT_EQUAL(this, that,"'x' should not be equal to 'y'");
+} // end case
+
+XTEST_CASE(basic_run_of_uint64) {
+    uint64_t this = 32, that = 22, other = 32;
+
+    XASSERT_UINT64_EQUAL(this, other,"'x' should be equal to 'y'");
+    XASSERT_UINT64_GREATER(this, that,"'x' should be greater than 'y'");
+    XASSERT_UINT64_GREATER_EQUAL(this, that,"'x' should be greater or equal to 'y'");
+    XASSERT_UINT64_LESS(that, other,"'x' should be lesser than 'y'");
+    XASSERT_UINT64_LESS_EQUAL(that, this,"'x' should be lesser or equal to 'y'");
+    XASSERT_UINT64_NOT_EQUAL(this, that,"'x' should not be equal to 'y'");
 } // end case
 
 XTEST_CASE(basic_run_of_hex) {
@@ -108,7 +197,15 @@ XTEST_CASE(basic_run_of_boolean) {
 void xfixture_basic_cases(XUnitRunner *runner)
 {
     xtest_run(&basic_run_of_int, runner);
+    xtest_run(&basic_run_of_int8, runner);
+    xtest_run(&basic_run_of_int16, runner);
+    xtest_run(&basic_run_of_int32, runner);
+    xtest_run(&basic_run_of_int64, runner);
     xtest_run(&basic_run_of_uint, runner);
+    xtest_run(&basic_run_of_uint8, runner);
+    xtest_run(&basic_run_of_uint16, runner);
+    xtest_run(&basic_run_of_uint32, runner);
+    xtest_run(&basic_run_of_uint64, runner);
     xtest_run(&basic_run_of_hex, runner);
     xtest_run(&basic_run_of_oct, runner);
     xtest_run(&basic_run_of_bit, runner);
