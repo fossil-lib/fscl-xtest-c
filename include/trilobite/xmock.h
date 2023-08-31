d0affe4ae88663b3489ab5ce99e2b66736f8d54e
Michael Gene Brockus (Dreamer) <55331536+dreamer-coding-555@users.noreply.github.com>
2023-08-30 19:27:05 -0600

    Update xmock.h

--- include/trilobite/xmock.h
+++ include/trilobite/xmock.h
@@ -14,21 +14,30 @@ extern "C"
 
 #include "xtest.h"
 
-//
-// STUB
-//
-typedef struct XMockStub
-{
-   void *(*func_ptr)(void *);
-   void *_result;
-   size_t _size;
-} XMockStub;
+// Define the mock function macro using _Generic
+#define XMOCK_FUNC_RETURN_TYPE(type) _Generic((type), \
+    int: int, \
+    int8_t: int8_t, \
+    int16_t: int16_t, \
+    int32_t: int32_t, \
+    int64_t: int64_t, \
+    unsigned int: unsigned int, \
+    uint8_t: uint8_t, \
+    uint16_t: uint16_t, \
+    uint32_t: uint32_t, \
+    uint64_t: uint64_t, \
+    float: float, \
+    double: double, \
+    char *: char *, \
+    default: void * \
+)
 
-XMockStub *xmock_stub_create(void *(*func_ptr)(void *), void *result, size_t size);
-void xmock_stub_erase(XMockStub *stub);
-void *xmock_stub_call(XMockStub *stub, void *arg);
+#define XMOCK_FUNC_RETVAL(type, value) (XMOCK_FUNC_RETURN_TYPE(type))(value)
 
-
+// Mocked function using _Generic
+#define XMOCK_FUNC(type, name) XMOCK_FUNC_RETURN_TYPE(type) xmock_##name(type arg) { \
+    return XMOCK_FUNC_RETVAL(type, arg); \
+} // end macro
 
 #ifdef __cplusplus
 }
