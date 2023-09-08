5f487b1871484b21b536e2efd86b38a963b0e874
Michael Gene Brockus (Dreamer) <55331536+dreamer-coding-555@users.noreply.github.com>
2023-09-08 15:04:24 -0600

    Update xauto.c

--- src/xauto.c
+++ src/xauto.c
@@ -5,3 +5,35 @@
    website: <https://trilobite.code.blog>
 */
 #include "trilobite/xauto.h"
+
+// Sigmoid function
+double xauto_sigmoid(double x) {
+    return 1.0 / (1.0 + exp(-x));
+} // end of func
+
+// Training function for Logistic Regression
+void xauto_train_logistic_regression(LogisticRegressionModel* model, const double* input, const int* labels, int num_samples, double learning_rate, int num_epochs) {
+    // Initialize model coefficients and intercept to zeros
+    for (int i = 0; i < FEATURES; ++i) {
+        model->coefficients[i] = 0.0;
+    } // end for
+    model->intercept = 0.0;
+
+    // Perform gradient descent
+    for (int epoch = 0; epoch < num_epochs; ++epoch) {
+        for (int sample = 0; sample < num_samples; ++sample) {
+            double prediction = model->intercept;
+            for (int feature = 0; feature < FEATURES; ++feature) {
+                prediction += model->coefficients[feature] * input[sample * FEATURES + feature];
+            } // end for
+            double probability = xauto_sigmoid(prediction);
+
+            // Update coefficients and intercept using gradient descent
+            double error = labels[sample] - probability;
+            model->intercept += learning_rate * error;
+            for (int feature = 0; feature < FEATURES; ++feature) {
+                model->coefficients[feature] += learning_rate * error * input[sample * FEATURES + feature];
+            } // end for
+        } // end for
+    } // end for
+} // end of func
