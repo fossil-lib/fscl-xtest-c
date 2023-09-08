71a95da0f45f461d4e28b8141872ae62a3f36c49
Michael Gene Brockus (Dreamer) <55331536+dreamer-coding-555@users.noreply.github.com>
2023-09-08 15:24:09 -0600

    Update xauto.c

--- src/xauto.c
+++ src/xauto.c
@@ -14,7 +14,7 @@ double xauto_sigmoid(double x) {
 // Training function for Logistic Regression
 void xauto_train_logistic_regression(LogisticRegressionModel* model, const double* input, const int* labels, int num_samples, double learning_rate, int num_epochs) {
     // Initialize model coefficients and intercept to zeros
-    for (int i = 0; i < FEATURES; ++i) {
+    for (int i = 0; i < model->num_features; ++i) {
         model->coefficients[i] = 0.0;
     } // end for
     model->intercept = 0.0;
@@ -23,16 +23,16 @@ void xauto_train_logistic_regression(LogisticRegressionModel* model, const doubl
     for (int epoch = 0; epoch < num_epochs; ++epoch) {
         for (int sample = 0; sample < num_samples; ++sample) {
             double prediction = model->intercept;
-            for (int feature = 0; feature < FEATURES; ++feature) {
-                prediction += model->coefficients[feature] * input[sample * FEATURES + feature];
+            for (int feature = 0; feature < model->num_features; ++feature) {
+                prediction += model->coefficients[feature] * input[sample * model->num_features + feature];
             } // end for
             double probability = xauto_sigmoid(prediction);
 
             // Update coefficients and intercept using gradient descent
             double error = labels[sample] - probability;
             model->intercept += learning_rate * error;
-            for (int feature = 0; feature < FEATURES; ++feature) {
-                model->coefficients[feature] += learning_rate * error * input[sample * FEATURES + feature];
+            for (int feature = 0; feature < model->num_features; ++feature) {
+                model->coefficients[feature] += learning_rate * error * input[sample * model->num_features + feature];
             } // end for
         } // end for
     } // end for
