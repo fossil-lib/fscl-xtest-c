375ea045fdea2d85c3d9ed48a31a6e2850b4f045
Michael Gene Brockus (Dreamer) <55331536+dreamer-coding-555@users.noreply.github.com>
2023-09-08 15:56:08 -0600

    Update xauto.c

--- src/xauto.c
+++ src/xauto.c
@@ -6,6 +6,9 @@
 */
 #include "trilobite/xauto.h"
 
+//
+// EXTRA MATH FUNCTIONS
+//
 double xauto_exp_with_squaring(double x, int n) {
     if (n == 0) {
         return 1.0;
@@ -24,6 +27,10 @@ double xauto_exp_with_squaring(double x, int n) {
 double xauto_sigmoid(double x) {
     return 1.0 / (1.0 + xauto_exp_with_squaring(-x, 10));
 } // end of func
+
+//
+// AI TRAINING
+//
 
 // Training function for Logistic Regression
 void xauto_train_logistic_regression(LogisticRegressionModel* model, const double* input, const int* labels, int num_samples, double learning_rate, int num_epochs) {
@@ -50,4 +57,28 @@ void xauto_train_logistic_regression(LogisticRegressionModel* model, const doubl
             } // end for
         } // end for
     } // end for
+} // end of func
+
+// Training function for Linear Regression
+void xauto_train_linear_regression(LinearRegressionModel* model, const double* x, const double* y, int num_samples) {
+    double sum_x = 0.0;
+    double sum_y = 0.0;
+    double sum_xy = 0.0;
+    double sum_x_squared = 0.0;
+
+    for (int i = 0; i < num_samples; ++i) {
+        sum_x += x[i];
+        sum_y += y[i];
+        sum_xy += x[i] * y[i];
+        sum_x_squared += x[i] * x[i];
+    } // end for
+
+    // Calculate the slope (gradient) and intercept (bias) of the line
+    double mean_x = sum_x / num_samples;
+    double mean_y = sum_y / num_samples;
+    double slope = (sum_xy - num_samples * mean_x * mean_y) / (sum_x_squared - num_samples * mean_x * mean_x);
+    double intercept = mean_y - slope * mean_x;
+
+    model->slope = slope;
+    model->intercept = intercept;
 } // end of func
