674d428258ca31174bdbc02aac59f134e33e85c8
Michael Gene Brockus (Dreamer) <55331536+dreamer-coding-555@users.noreply.github.com>
2023-09-08 15:02:03 -0600

    Create xauto.c

---
+++ src/xauto.c
@@ -0,0 +1,7 @@
+/*
+   under:   trilobite stdlib
+   author:  Michael Gene Brockus (Dreamer)
+   gmail:   <michaelbrockus@gmail.com>
+   website: <https://trilobite.code.blog>
+*/
+#include "trilobite/xauto.h"
