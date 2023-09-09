fdd239a6673c74db73cf6eb68e98dc16c55c2988
Michael Gene Brockus (Dreamer) <55331536+dreamer-coding-555@users.noreply.github.com>
2023-09-09 10:48:17 -0600

    Update xtest.c

--- src/xtest.c
+++ src/xtest.c
@@ -6,10 +6,6 @@
 */
 #include "trilobite/xtest.h"
 
-// Static array to hold test cases
-static bool XEXPECT_PASS_SCAN = true;
-static bool XASSERT_PASS_SCAN = true;
-
 // ANSI escape code macros for text color
 #define ANSI_COLOR_RED     "\x1b[31m"
 #define ANSI_COLOR_GREEN   "\x1b[32m"
@@ -17,6 +13,169 @@ static bool XASSERT_PASS_SCAN = true;
 #define ANSI_COLOR_WHITE   "\x1b[37m"
 #define ANSI_COLOR_YELLOW  "\x1b[33m"
 #define ANSI_COLOR_RESET   "\x1b[0m"
+
+// Static array to hold test cases
+static bool XEXPECT_PASS_SCAN = true;
+static bool XASSERT_PASS_SCAN = true;
+
+static bool XTEST_FLAG_VERBOSE    = false;
+static bool XTEST_FLAG_VERSION    = false;
+static bool XTEST_FLAG_COLORED    = false;
+static bool XTEST_FLAG_HELP       = false;
+static bool XTEST_FLAG_ONLY_TESTS = false;
+static bool XTEST_FLAG_ONLY_BENCH = false;
+
+CLIOption options[] = {
+     { "--verbose",    "-m", "Show more information to standard output", &XTEST_FLAG_VERBOSE },
+     { "--version",    "-v", "Get the version of this test framework", &XTEST_FLAG_VERSION },
+     { "--color"  ,    "-c", "Enable color text output", &XTEST_FLAG_COLORED },
+     { "--help",       "-h", "Print this message you see before you're eyes", &XTEST_FLAG_HELP },
+     { "--only-tests", "-t", "Only run unit test cases", &XTEST_FLAG_ONLY_TESTS },
+     { "--only-bench", "-b", "Only run benchmark cases", &XTEST_FLAG_ONLY_BENCH }
+ }; // end of command-line options
+
+// Function to print usage instructions, including custom options
+void xtest_cli_print_usage(const char* program_name, const XTestCliOption* options, size_t num_options) {
+    printf("Usage: %s [options]\n", program_name);
+    printf("Options:\n");
+
+    for (size_t i = 0; i < num_options; ++i) {
+        printf("  %s\t%s\n", options[i].option_name_long, options[i].option_name_short, options[i].description);
+    } // end for
+    puts("########################################");
+} // end of func
+
+// Function to parse command-line arguments and set custom option flags
+int xtest_cli_parse_args(XTestCliOption* options, size_t num_options, int argc, char** argv) {
+    for (int i = 1; i < argc; ++i) {
+        for (size_t j = 0; j < num_options; ++j) {
+            if (strcmp(argv[i], options[j].option_name) == 0) {
+                *(options[j].flag) = 1;
+                break;
+            } // end if
+        } // end for
+    } // end for
+    return 0;
+} // end of func
+
+XUnitRunner xtest_start(int argc, char **argv) {
+    XUnitRunner runner;
+    xtest_cli_parse_args(options, num_options, argc, (char**)argv);
+    runner.verbose = XTEST_FLAG_VERBOSE;       // --verbose, -v : Show more information to standard output
+    runner.version = XTEST_FLAG_VERSION;       // --version     : Get the version of this test framework
+    runner.color = XTEST_FLAG_COLORED;         // --color, -c   : Enable color text output 
+    runner.help = XTEST_FLAG_HELP;             // --help, -h    : Print this message you see before you're eyes
+    runner.only_tests = XTEST_FLAG_ONLY_TESTS; // --only-tests  : Only run unit test cases
+    runner.only_bench = XTEST_FLAG_ONLY_BENCH; // --only-bench  : Only run benchmark cases
+
+    // TODO: process args here
+   
+    runner.stats = (XTestStats){0, 0, 0, 0, 0, 0};
+    return runner;
+} // end of func
+
+int xtest_end(XUnitRunner *runner) {
+    printf("%s[TRILOBITE XUNIT RUNNER] results of the test%s", ANSI_COLOR_BLUE, ANSI_COLOR_RESET);
+    printf("Tests passed: %d\n", runner->stats.passed_count);
+    printf("Tests failed: %d\n", runner->stats.failed_count);
+    printf("Tests ignored: %d\n", runner->stats.ignored_count);
+    printf("Total count: %d\n", runner->stats.total_count);
+    return runner->stats.failed_count;
+} // end of func
+
+void xtest_run_unit(const XTestCase* test_case, XTestStats* stats)  {
+    // Execute the test function
+    if (!test_case->ignored) {
+        clock_t start_time = clock(); // Record start time
+
+        // ... Execute the test function
+        // Example:
+        // test_case->test_function();
+
+        clock_t end_time = clock(); // Record end time
+
+        // Calculate elapsed time and store it in the test_case
+        test_case->elapsed_time = end_time - start_time;
+
+        // Determine whether the test passed or failed based on your logic
+        bool test_passed = true; // Assume the test passes by default
+
+        // Check whether expectation scanning is enabled
+        if (!XEXPECT_PASS_SCAN) {
+            // If any expectations fail, consider the test as failed
+            test_passed = false;
+        } // end if
+
+        // Check whether assertion scanning is enabled
+        if (!XASSERT_PASS_SCAN) {
+            // If any assertions fail, consider the test as failed
+            test_passed = false;
+        } // end if
+
+        // Update the appropriate count based on the test result
+        if (test_passed) {
+            stats->passed_count++;
+        } else {
+            stats->failed_count++;
+        } // end if else
+    } else {
+        // Update the ignored count
+        stats->ignored_count++;
+    } // end if else
+
+    // Update the total count
+    stats->total_count++;
+} // end of func
+
+void xtest_run_fixture(const XTestCase* test_case, const XTestFixture* fixture, XTestStats* stats)  {
+    // Execute the test function within the fixture
+    if (!test_case->ignored) {
+        clock_t start_time = clock(); // Record start time
+
+        if (fixture->setup) {
+            fixture->setup();
+        } // end if
+   
+        test_case->test_function();
+   
+        if (fixture->teardown) {
+            fixture->teardown();
+        } // end if
+
+        clock_t end_time = clock(); // Record end time
+
+        // Calculate elapsed time and store it in the test_case
+        test_case->elapsed_time = end_time - start_time;
+
+        // Determine whether the test passed or failed based on your logic
+        bool test_passed = true; // Assume the test passes by default
+
+        // Check whether expectation scanning is enabled
+        if (!XEXPECT_PASS_SCAN) {
+            // If any expectations fail, consider the test as failed
+            test_passed = false;
+        } // end if
+
+        // Check whether assertion scanning is enabled
+        if (!XASSERT_PASS_SCAN) {
+            // If any assertions fail, consider the test as failed
+            test_passed = false;
+        } // end if
+
+        // Update the appropriate count based on the test result
+        if (test_passed) {
+            stats->passed_count++;
+        } else {
+            stats->failed_count++;
+        } // end if else
+    } else {
+        // Update the ignored count
+        stats->ignored_count++;
+    } // end if else
+
+    // Update the total count
+    stats->total_count++;
+} // end of func
 
 /**
     @brief Starter for the Xunit test runner which will process commands
@@ -65,113 +224,6 @@ XUnitRunner xtest_start(int argc, char **argv)
     return runner;
 } // end of func
 
-/**
-    @brief This function checks the result of the XUnitRunner and displays a
-    message based on the result.
-
-    @param runner The XUnitRunner object that is being checked
-    @returns The number of failed tests that were run
-*/
-int xtest_end(XUnitRunner *runner)
-{
-    if (runner->failed_count > 0) {
-        puts(ANSI_COLOR_RED "RUNNER: Failed" ANSI_COLOR_RESET);
-    } else if (runner->passed_count == 0 && runner->failed_count == 0) {
-        puts(ANSI_COLOR_YELLOW "RUNNER: Not sure what to do" ANSI_COLOR_RESET);
-    } // end if else if
-    else if (runner->passed_count > 0 || runner->failed_count == 0) {
-        puts(ANSI_COLOR_GREEN "RUNNER: Passed" ANSI_COLOR_RESET);
-    }
-    
-    printf(ANSI_COLOR_BLUE "SCORE: PASS [%2d] FAIL [%2d]\n\n" ANSI_COLOR_RESET, runner->passed_count, runner->failed_count);
-    return runner->failed_count;
-} // end of func
-
-/**
-    @brief This function runs a test case and prints the results.
-
-    @param test_case: A pointer to a XTestCase object containing
-                      the test case to be run.
-    @param runner: A pointer to a XUnitRunner object containing
-                   the runner configuration.
-    @returns void: No value is returned.
-*/
-void xtest_run(XTestCase *test_case, XUnitRunner *runner) {
-   if (strcmp(runner->run_tag, "bench") == 0) {
-       return;
-   } // end if
-
-    printf(ANSI_COLOR_BLUE "Running test: " ANSI_COLOR_YELLOW " %s\n" ANSI_COLOR_RESET, test_case->name);
-
-    if (runner->setup_function) {
-        runner->setup_function();
-    } // end if
-
-    test_case->test_function();
-
-    if (runner->teardown_function) {
-        runner->teardown_function();
-    } // end if
-
-
-    puts("Assertions: DONE");
-    if (XASSERT_PASS_SCAN == true || XEXPECT_PASS_SCAN == true) {
-        runner->passed_count++;
-    } else {
-        runner->failed_count++;
-    } // end if else
-    putchar('\n');
-} // end of func
-
-/**
-  @brief This function xbench_run runs a benchmark and measures its elapsed time.
-
-  @param XBench *benchmark: a pointer to the benchmark to be run
-  @param XUnitRunner *runner: a pointer to the unit test runner
-
-  @returns: void - the elapsed time of the benchmark is stored in benchmark->elapsed_time
-*/
-void xbench_run(XBench *benchmark, XUnitRunner *runner) {
-    if (strcmp(runner->run_tag, "test") == 0) {
-        return;
-    } // end if
-
-    printf(ANSI_COLOR_BLUE "Running benchmark: " ANSI_COLOR_YELLOW " %s\n" ANSI_COLOR_RESET, benchmark->name);
-
-    if (runner->setup_function) {
-        runner->setup_function();
-    } // end if
-
-    clock_t start_time = clock();
-    benchmark->benchmark_function();
-    clock_t end_time = clock();
-
-    if (runner->teardown_function) {
-        runner->teardown_function();
-    } // end if
-
-    benchmark->elapsed_time = end_time - start_time;
-
-    printf(ANSI_COLOR_GREEN "Elapsed time:" ANSI_COLOR_RESET " %.6f seconds\n", (double)benchmark->elapsed_time / CLOCKS_PER_SEC);
-    puts("Benchmark: DONE\n");
-
-
-    runner->passed_count++;
-} // end of func
-
-/**
-    @brief Sets the setup and teardown functions for the given test case.
-
-    @param test_case The test case to set the setup and teardown functions for.
-    @param setup_func The function to be called before the test is run.
-    @param teardown_func The function to be called after the test is complete.
-
-    @returns Nothing.
-*/
-void xtest_set_setup_teardown(XUnitRunner *runner, void (*setup_func)(void), void (*teardown_func)(void)) {
-    runner->setup_function = setup_func;
-    runner->teardown_function = teardown_func;
-} // end of func
 
 /**
     @brief Asserts that the expression is true, and prints a message if it is not.
