94eafecd4e43a732525062e11e4b22d492de24d8
Michael Gene Brockus (Dreamer) <55331536+dreamer-coding-555@users.noreply.github.com>
2023-09-10 17:36:03 -0500

    Create xignore.h

---
+++ include/trilobite/xignore.h
@@ -0,0 +1,22 @@
+/*
+   under:   trilobite stdlib
+   author:  Michael Gene Brockus (Dreamer)
+   gmail:   <michaelbrockus@gmail.com>
+   website: <https://trilobite.code.blog>
+*/
+#ifndef TRILOBITE_XIGNORE_H
+#define TRILOBITE_XIGNORE_H
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+#include "xtest.h"
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
