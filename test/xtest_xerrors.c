80b15be92d9656caf9454cf25d103409833d9fc6
Michael Gene Brockus <michaelbrockus@gmail.com>
2023-09-11 11:35:33 -0600

    write test for xerror

--- test/xtest_xerrors.c
+++ test/xtest_xerrors.c
@@ -8,3 +8,47 @@
 #include "trilobite/xtest.h"
 #include "trilobite/xerrors.h"
 
+//
+// TEST FIXTURE
+//
+XTEST_FIXTURE(xerrors_test_fixture);
+
+XTEST_SETUP(xerrors_test_fixture) {
+    // empty
+} // end setup
+
+XTEST_TEARDOWN(xerrors_test_fixture) {
+    // empty
+} // end teardown
+
+//
+// TEST CASES
+//
+XTEST_CASE_FIXTURE(xerrors_test_fixture, xerrors_do_throw) {
+    XERRORS_EXPECT_THROW(xerrors_throw("Exception", "Test"), "Exception", "Test");
+    XERRORS_EXPECT_THROW(XERRORS_THROW("Exception", "Test"), "Exception", "Test");
+} // end case
+
+XTEST_CASE_FIXTURE(xerrors_test_fixture, xerrors_no_throw) {
+    XERRORS_EXPECT_NO_THROW(printf("No exception\n"));
+} // end case
+
+XTEST_CASE_FIXTURE(xerrors_test_fixture, xerrors_throw_with_message) {
+    XERRORS_EXPECT_THROW_WITH_MESSAGE(xerrors_throw("Exception", "Test"), "Exception", "Test", "Custom message");
+    XERRORS_EXPECT_THROW_WITH_MESSAGE(XERRORS_THROW("Exception", "Test"), "Exception", "Test", "Custom message");
+} // end case
+
+XTEST_CASE_FIXTURE(xerrors_test_fixture, xerrors_custom_error) {
+    XERRORS_EXPECT_CUSTOM_ERROR(xerrors_throw("CustomError", "Test"), "CustomError", "Test");
+    XERRORS_EXPECT_CUSTOM_ERROR(XERRORS_THROW("CustomError", "Test"), "CustomError", "Test");
+} // end case
+
+//
+// LOCAL TEST GROUP
+//
+void xignore_test_group(XUnitRunner *runner) {
+    xtest_run_test_fixture(&xerrors_do_throw,           &xerrors_test_fixture, &runner->stats);
+    xtest_run_test_fixture(&xerrors_no_throw,           &xerrors_test_fixture, &runner->stats);
+    xtest_run_test_fixture(&xerrors_throw_with_message, &xerrors_test_fixture, &runner->stats);
+    xtest_run_test_fixture(&xerrors_custom_error,       &xerrors_test_fixture, &runner->stats);
+} // end of group
