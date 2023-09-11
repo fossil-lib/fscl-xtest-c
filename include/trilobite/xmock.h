31381a862fd8e9557cd9592bedf01446334d00bc
Michael Gene Brockus <michaelbrockus@gmail.com>
2023-09-10 19:12:28 -0600

    adding mock structures

--- include/trilobite/xmock.h
+++ include/trilobite/xmock.h
@@ -44,6 +44,267 @@ typedef void *xmock_default;
 // This macro provides a way to define mocked functions with specified return types and arguments.
 #define XMOCK_FUNC(type, name) type xmock_##name(type arg)
 
+/**
+ * @file mock_data_structures.h
+ *
+ * @brief Mock Data Structures
+ *
+ * This file defines a set of mock data structures and associated functions for educational
+ * purposes. These data structures are simplified versions of common data structures used
+ * in computer science and software development.
+ *
+ * 1. Mock Stack:
+ *    - The `XMockStack` structure represents a stack, a Last-In, First-Out (LIFO) data structure.
+ *    - It consists of an array (`data`) to store stack elements, the current size (`size`) of
+ *      the stack, and the maximum capacity (`capacity`) of the stack.
+ *    - Functions:
+ *      - `XMockStack* xmock_stack_create(int capacity)`:
+ *        Creates and initializes a mock stack with the given capacity.
+ *      - `void xmock_stack_push(XMockStack* stack, int element)`:
+ *        Pushes an element onto the stack.
+ *      - `int xmock_stack_pop(XMockStack* stack)`:
+ *        Pops an element from the stack.
+ *      - `int xmock_stack_size(XMockStack* stack)`:
+ *        Returns the current size of the stack.
+ *      - `void xmock_stack_destroy(XMockStack* stack)`:
+ *        Destroys the mock stack and releases allocated resources.
+ *
+ * 2. Mock Doubly Linked List:
+ *    - The `XMockDoublyLinkedList` structure represents a doubly linked list.
+ *    - It consists of nodes (`XMockNode`) with integer data, where each node has pointers to
+ *      the previous (`prev`) and next (`next`) nodes.
+ *    - The linked list maintains pointers to the `head` (first node) and `tail` (last node),
+ *      as well as the current size (`size`) of the list.
+ *    - Functions:
+ *      - `XMockDoublyLinkedList* xmock_doubly_linked_list_create()`:
+ *        Creates and initializes a mock doubly linked list.
+ *      - `void xmock_doubly_linked_list_push_front(XMockDoublyLinkedList* list, int data)`:
+ *        Inserts an element at the beginning of the list.
+ *      - `void xmock_doubly_linked_list_push_back(XMockDoublyLinkedList* list, int data)`:
+ *        Inserts an element at the end of the list.
+ *      - `int xmock_doubly_linked_list_pop_front(XMockDoublyLinkedList* list)`:
+ *        Removes and returns the first element of the list.
+ *      - `int xmock_doubly_linked_list_pop_back(XMockDoublyLinkedList* list)`:
+ *        Removes and returns the last element of the list.
+ *      - `int xmock_doubly_linked_list_size(XMockDoublyLinkedList* list)`:
+ *        Returns the current size of the list.
+ *      - `void xmock_doubly_linked_list_destroy(XMockDoublyLinkedList* list)`:
+ *        Destroys the mock doubly linked list and releases allocated resources.
+ *
+ * 3. Mock Queue:
+ *    - The `XMockQueue` structure represents a queue, a First-In, First-Out (FIFO) data structure.
+ *    - It consists of an array (`data`) to store queue elements, pointers to the `front` and `rear`
+ *      indices, the current size (`size`) of the queue, and the maximum capacity (`capacity`) of
+ *      the queue.
+ *    - Functions:
+ *      - `XMockQueue* xmock_queue_create(int capacity)`:
+ *        Creates and initializes a mock queue with the given capacity.
+ *      - `void xmock_queue_enqueue(XMockQueue* queue, int element)`:
+ *        Enqueues an element into the queue.
+ *      - `int xmock_queue_dequeue(XMockQueue* queue)`:
+ *        Dequeues and returns the front element of the queue.
+ *      - `int xmock_queue_size(XMockQueue* queue)`:
+ *        Returns the current size of the queue.
+ *      - `void xmock_queue_destroy(XMockQueue* queue)`:
+ *        Destroys the mock queue and releases allocated resources.
+ *
+ * 4. Mock Map:
+ *    - The `XMockMap` structure represents a simple key-value map.
+ *    - It consists of an array of key-value pairs (`entries`), the current size (`size`) of the map,
+ *      and a maximum size defined by `XMOCK_MAP_MAX_SIZE`.
+ *    - Functions:
+ *      - `XMockMap* xmock_map_create()`:
+ *        Creates and initializes a mock map.
+ *      - `void xmock_map_put(XMockMap* map, int key, int value)`:
+ *        Puts a key-value pair into the map.
+ *      - `int xmock_map_get(XMockMap* map, int key)`:
+ *        Gets the value associated with a key from the map.
+ *      - `int xmock_map_contains(XMockMap* map, int key)`:
+ *        Checks if a key exists in
+ */
+
+//
+// MOCK STACK
+//
+
+// Define the mock stack structure
+typedef struct {
+    int* data;      // Array to store stack elements
+    int size;       // Current size of the stack
+    int capacity;   // Maximum capacity of the stack
+} XMockStack;
+
+// Create and initialize a mock stack
+// @param capacity: Maximum capacity of the stack
+// @return: A pointer to the newly created stack
+XMockStack* xmock_stack_create(int capacity);
+
+// Push an element onto the mock stack
+// @param stack: Pointer to the stack
+// @param element: Element to be pushed onto the stack
+void xmock_stack_push(XMockStack* stack, int element);
+
+// Pop an element from the mock stack
+// @param stack: Pointer to the stack
+// @return: The popped element
+int xmock_stack_pop(XMockStack* stack);
+
+// Get the size of the mock stack
+// @param stack: Pointer to the stack
+// @return: Current size of the stack
+int xmock_stack_size(XMockStack* stack);
+
+// Destroy the mock stack and release resources
+// @param stack: Pointer to the stack to be destroyed
+void xmock_stack_destroy(XMockStack* stack);
+
+
+//
+// MOCK LINKED LIST
+//
+
+// Define the mock doubly linked list node structure
+typedef struct XMockNode {
+    int data;                   // Data stored in the node
+    struct XMockNode* prev;     // Pointer to the previous node
+    struct XMockNode* next;     // Pointer to the next node
+} XMockNode;
+
+// Define the mock doubly linked list structure
+typedef struct {
+    XMockNode* head;            // Pointer to the first node
+    XMockNode* tail;            // Pointer to the last node
+    int size;                   // Current size of the list
+} XMockDoublyLinkedList;
+
+// @brief Create and initialize a mock doubly linked list.
+// @return A pointer to the created mock doubly linked list or NULL if allocation fails.
+XMockDoublyLinkedList* xmock_doubly_linked_list_create();
+
+// @brief Insert an element at the beginning of the list.
+// @param list The mock doubly linked list to which the element will be added.
+// @param data The data to insert.
+void xmock_doubly_linked_list_push_front(XMockDoublyLinkedList* list, int data);
+
+// @brief Insert an element at the end of the list.
+// @param list The mock doubly linked list to which the element will be added.
+// @param data The data to insert.
+void xmock_doubly_linked_list_push_back(XMockDoublyLinkedList* list, int data);
+
+// @brief Remove and return the first element of the list.
+// @param list The mock doubly linked list from which the first element will be removed.
+// @return The value of the first element or 0 if the list is empty.
+int xmock_doubly_linked_list_pop_front(XMockDoublyLinkedList* list);
+
+// @brief Remove and return the last element of the list.
+// @param list The mock doubly linked list from which the last element will be removed.
+// @return The value of the last element or 0 if the list is empty.
+int xmock_doubly_linked_list_pop_back(XMockDoublyLinkedList* list);
+
+// @brief Get the size of the list.
+// @param list The mock doubly linked list for which to determine the size.
+// @return The current size of the list.
+int xmock_doubly_linked_list_size(XMockDoublyLinkedList* list);
+
+// @brief Destroy the mock doubly linked list and release allocated resources.
+// @param list The mock doubly linked list to destroy.
+void xmock_doubly_linked_list_destroy(XMockDoublyLinkedList* list);
+
+
+//
+// XMOCK QUEUE
+//
+
+// @brief Define the mock queue structure.
+typedef struct {
+    int* data;       // @brief Array to store queue elements.
+    int front;       // @brief Index of the front element.
+    int rear;        // @brief Index of the rear element.
+    int size;        // @brief Current size of the queue.
+    int capacity;    // @brief Maximum capacity of the queue.
+} XMockQueue;
+
+// @brief Create and initialize a mock queue.
+// @param capacity The maximum capacity of the queue.
+// @return A pointer to the created mock queue or NULL if allocation fails.
+XMockQueue* xmock_queue_create(int capacity);
+
+// @brief Enqueue an element into the mock queue.
+// @param queue The mock queue to which the element will be enqueued.
+// @param element The element to be enqueued.
+void xmock_queue_enqueue(XMockQueue* queue, int element);
+
+// @brief Dequeue and return the front element of the mock queue.
+// @param queue The mock queue from which the front element will be dequeued.
+// @return The front element of the queue or 0 if the queue is empty.
+int xmock_queue_dequeue(XMockQueue* queue);
+
+// @brief Get the size of the mock queue.
+// @param queue The mock queue for which to determine the size.
+// @return The current size of the queue.
+int xmock_queue_size(XMockQueue* queue);
+
+// @brief Destroy the mock queue and release allocated resources.
+// @param queue The mock queue to destroy.
+void xmock_queue_destroy(XMockQueue* queue);
+
+
+
+//
+// XMOCK MAP
+//
+
+// @brief Define the maximum number of key-value pairs the mock map can hold.
+#define XMOCK_MAP_MAX_SIZE 100
+
+// @brief Define the key-value pair structure.
+typedef struct {
+    int key;    // @brief The key of the entry.
+    int value;  // @brief The value associated with the key.
+} XMockMapEntry;
+
+// @brief Define the mock map structure.
+typedef struct {
+    XMockMapEntry entries[XMOCK_MAP_MAX_SIZE];  // @brief Array of key-value pairs.
+    int size;                                   // @brief Current number of entries in the map.
+} XMockMap;
+
+// @brief Create and initialize a mock map.
+XMockMap* xmock_map_create();
+
+// @brief Put a key-value pair into the mock map.
+// @param map The mock map to which the entry will be added.
+// @param key The key of the entry to add.
+// @param value The value associated with the key.
+void xmock_map_put(XMockMap* map, int key, int value);
+
+// @brief Get the value associated with a key from the mock map.
+// @param map The mock map to search for the key.
+// @param key The key to look up in the map.
+// @return The value associated with the key or 0 if the key is not found.
+int xmock_map_get(XMockMap* map, int key);
+
+// @brief Check if a key exists in the mock map.
+// @param map The mock map to check for the key.
+// @param key The key to check for existence.
+// @return 1 if the key exists in the map, 0 otherwise.
+int xmock_map_contains(XMockMap* map, int key);
+
+// @brief Remove a key-value pair from the mock map.
+// @param map The mock map from which the entry will be removed.
+// @param key The key of the entry to remove.
+void xmock_map_remove(XMockMap* map, int key);
+
+// @brief Get the number of key-value pairs in the mock map.
+// @param map The mock map for which to count the entries.
+// @return The number of key-value pairs in the map.
+int xmock_map_size(XMockMap* map);
+
+// @brief Destroy the mock map and release allocated resources.
+// @param map The mock map to destroy.
+void xmock_map_destroy(XMockMap* map);
+
+
 #ifdef __cplusplus
 }
 #endif
---
+++ src/xmock.c
@@ -0,0 +1,461 @@
+/*
+   under:   trilobite stdlib
+   author:  Michael Gene Brockus (Dreamer)
+   gmail:   <michaelbrockus@gmail.com>
+   website: <https://trilobite.code.blog>
+*/
+#include "trilobite/xmock.h"
+
+/**
+ * @brief Creates a new XMockStack with the specified capacity.
+ *
+ * @param capacity The maximum number of elements the stack can hold.
+ *
+ * @return A pointer to the newly created XMockStack if successful, or NULL if memory allocation fails.
+ */
+XMockStack* xmock_stack_create(int capacity) {
+    // Allocate memory for the stack structure
+    XMockStack* stack = (XMockStack*)malloc(sizeof(XMockStack));
+
+    if (stack == NULL) {
+        return NULL; // Allocation failed
+    } // end if
+
+    // Allocate memory for the stack's data array
+    stack->data = (int*)malloc(sizeof(int) * capacity);
+
+    if (stack->data == NULL) {
+        free(stack);
+        return NULL; // Allocation failed
+    } // end if
+
+    // Initialize stack properties
+    stack->size = 0;
+    stack->capacity = capacity;
+
+    return stack;
+} // end of func
+
+/**
+ * @brief Pushes an integer element onto the stack.
+ *
+ * @param stack A pointer to the XMockStack.
+ * @param element The integer element to push onto the stack.
+ */
+void xmock_stack_push(XMockStack* stack, int element) {
+    if (stack->size < stack->capacity) {
+        // Push the element onto the stack if it's not full
+        stack->data[stack->size++] = element;
+    } // end if
+} // end of func
+
+/**
+ * @brief Pops and returns the top element from the stack.
+ *
+ * @param stack A pointer to the XMockStack.
+ *
+ * @return The popped integer element if the stack is not empty, or a default value of 0 if the stack is empty.
+ */
+int xmock_stack_pop(XMockStack* stack) {
+    if (stack->size > 0) {
+        // Pop and return the top element if the stack is not empty
+        return stack->data[--stack->size];
+    } // end if
+    
+    // Default return value for an empty stack (you can customize this behavior)
+    return 0;
+} // end of func
+
+/**
+ * @brief Returns the current size (number of elements) of the stack.
+ *
+ * @param stack A pointer to the XMockStack.
+ *
+ * @return The current size of the stack.
+ */
+int xmock_stack_size(XMockStack* stack) {
+    return stack->size;
+} // end of func
+
+/**
+ * @brief Destroys the XMockStack and frees the allocated memory.
+ *
+ * @param stack A pointer to the XMockStack to be destroyed.
+ */
+void xmock_stack_destroy(XMockStack* stack) {
+    if (stack != NULL) {
+        // Free memory for the data array and the stack structure
+        free(stack->data);
+        free(stack);
+    } // end if
+} // end of func
+
+//
+// LINKED LIST
+//
+
+/**
+ * @brief Creates a new XMockDoublyLinkedList.
+ *
+ * @return A pointer to the newly created XMockDoublyLinkedList if successful, or NULL if memory allocation fails.
+ */
+XMockDoublyLinkedList* xmock_doubly_linked_list_create() {
+    // Allocate memory for the linked list structure
+    XMockDoublyLinkedList* list = (XMockDoublyLinkedList*)malloc(sizeof(XMockDoublyLinkedList));
+
+    if (list == NULL) {
+        return NULL; // Allocation failed
+    } // end if
+
+    // Initialize linked list properties
+    list->head = NULL;
+    list->tail = NULL;
+    list->size = 0;
+
+    return list;
+} // end of func
+
+/**
+ * @brief Pushes an integer element to the front of the linked list.
+ *
+ * @param list A pointer to the XMockDoublyLinkedList.
+ * @param data The integer data to push to the front of the linked list.
+ */
+void xmock_doubly_linked_list_push_front(XMockDoublyLinkedList* list, int data) {
+    // Allocate memory for a new list node
+    XMockNode* newNode = (XMockNode*)malloc(sizeof(XMockNode));
+
+    if (newNode == NULL) {
+        return; // Allocation failed
+    } // end if
+
+    newNode->data = data;
+    newNode->prev = NULL;
+    newNode->next = list->head;
+
+    if (list->head != NULL) {
+        list->head->prev = newNode;
+    } // end if
+
+    list->head = newNode;
+
+    if (list->tail == NULL) {
+        list->tail = newNode;
+    } // end if
+
+    list->size++;
+} // end of func
+
+/**
+ * @brief Pushes an integer element to the back of the linked list.
+ *
+ * @param list A pointer to the XMockDoublyLinkedList.
+ * @param data The integer data to push to the back of the linked list.
+ */
+void xmock_doubly_linked_list_push_back(XMockDoublyLinkedList* list, int data) {
+    // Allocate memory for a new list node
+    XMockNode* newNode = (XMockNode*)malloc(sizeof(XMockNode));
+
+    if (newNode == NULL) {
+        return; // Allocation failed
+    } // end if
+
+    newNode->data = data;
+    newNode->prev = list->tail;
+    newNode->next = NULL;
+
+    if (list->tail != NULL) {
+        list->tail->next = newNode;
+    } // end if
+
+    list->tail = newNode;
+
+    if (list->head == NULL) {
+        list->head = newNode;
+    } // end if
+
+    list->size++;
+} // end of func
+
+/**
+ * @brief Pops and returns the front element of the linked list.
+ *
+ * @param list A pointer to the XMockDoublyLinkedList.
+ *
+ * @return The popped integer element if the list is not empty, or a default value of 0 if the list is empty.
+ */
+int xmock_doubly_linked_list_pop_front(XMockDoublyLinkedList* list) {
+    if (list->head == NULL) {
+        return 0; // Default return value for an empty list (you can customize this behavior)
+    } // end if
+
+    XMockNode* frontNode = list->head;
+    int data = frontNode->data;
+
+    list->head = frontNode->next;
+
+    if (list->head != NULL) {
+        list->head->prev = NULL;
+    } // end if
+
+    free(frontNode);
+    list->size--;
+
+    return data;
+} // end of func
+
+/**
+ * @brief Pops and returns the back element of the linked list.
+ *
+ * @param list A pointer to the XMockDoublyLinkedList.
+ *
+ * @return The popped integer element if the list is not empty, or a default value of 0 if the list is empty.
+ */
+int xmock_doubly_linked_list_pop_back(XMockDoublyLinkedList* list) {
+    if (list->tail == NULL) {
+        return 0; // Default return value for an empty list (you can customize this behavior)
+    } // end if
+
+    XMockNode* backNode = list->tail;
+    int data = backNode->data;
+
+    list->tail = backNode->prev;
+
+    if (list->tail != NULL) {
+        list->tail->next = NULL;
+    } // end if
+
+    free(backNode);
+    list->size--;
+
+    return data;
+} // end of func
+
+/**
+ * @brief Returns the current size (number of elements) of the linked list.
+ *
+ * @param list A pointer to the XMockDoublyLinkedList.
+ *
+ * @return The current size of the linked list.
+ */
+int xmock_doubly_linked_list_size(XMockDoublyLinkedList* list) {
+    return list->size;
+} // end of func
+
+/**
+ * @brief Destroys the XMockDoublyLinkedList and frees the allocated memory.
+ *
+ * @param list A pointer to the XMockDoublyLinkedList to be destroyed.
+ */
+void xmock_doubly_linked_list_destroy(XMockDoublyLinkedList* list) {
+    if (list != NULL) {
+        // Free memory for each list node and the linked list structure
+        XMockNode* current = list->head;
+        while (current != NULL) {
+            XMockNode* next = current->next;
+            free(current);
+            current = next;
+        } // end while
+        free(list);
+    } // end if
+} // end of func
+
+//
+//
+//
+/**
+ * @brief Creates a new XMockQueue with the specified capacity.
+ *
+ * @param capacity The maximum number of elements the queue can hold.
+ *
+ * @return A pointer to the newly created XMockQueue if successful, or NULL if memory allocation fails.
+ */
+XMockQueue* xmock_queue_create(int capacity) {
+    // Allocate memory for the queue structure
+    XMockQueue* queue = (XMockQueue*)malloc(sizeof(XMockQueue));
+
+    if (queue == NULL) {
+        return NULL; // Allocation failed
+    } // end if
+
+    // Allocate memory for the queue data
+    queue->data = (int*)malloc(sizeof(int) * capacity);
+
+    if (queue->data == NULL) {
+        free(queue);
+        return NULL; // Allocation failed
+    } // end if
+
+    // Initialize queue properties
+    queue->front = 0;
+    queue->rear = -1;
+    queue->size = 0;
+    queue->capacity = capacity;
+
+    return queue;
+} // end of func
+
+/**
+ * @brief Enqueues an integer element into the XMockQueue.
+ *
+ * @param queue A pointer to the XMockQueue.
+ * @param element The integer element to enqueue.
+ */
+void xmock_queue_enqueue(XMockQueue* queue, int element) {
+    if (queue->size < queue->capacity) {
+        queue->rear = (queue->rear + 1) % queue->capacity;
+        queue->data[queue->rear] = element;
+        queue->size++;
+    } // end if
+} // end of func
+
+/**
+ * @brief Dequeues and returns the front element from the XMockQueue.
+ *
+ * @param queue A pointer to the XMockQueue.
+ *
+ * @return The dequeued integer element if the queue is not empty, or a default value of 0 if the queue is empty.
+ */
+int xmock_queue_dequeue(XMockQueue* queue) {
+    if (queue->size > 0) {
+        int frontElement = queue->data[queue->front];
+        queue->front = (queue->front + 1) % queue->capacity;
+        queue->size--;
+        return frontElement;
+    } // end if
+    return 0; // Default return value for an empty queue (you can customize this behavior)
+} // end of func
+
+/**
+ * @brief Returns the current size (number of elements) of the XMockQueue.
+ *
+ * @param queue A pointer to the XMockQueue.
+ *
+ * @return The current size of the XMockQueue.
+ */
+int xmock_queue_size(XMockQueue* queue) {
+    return queue->size;
+} // end of func
+
+/**
+ * @brief Destroys the XMockQueue and frees the allocated memory.
+ *
+ * @param queue A pointer to the XMockQueue to be destroyed.
+ */
+void xmock_queue_destroy(XMockQueue* queue) {
+    if (queue != NULL) {
+        free(queue->data);
+        free(queue);
+    } // end if
+} // end of func
+
+//
+//
+//
+/**
+ * @brief Creates a new XMockMap.
+ *
+ * @return A pointer to the newly created XMockMap if successful, or NULL if memory allocation fails.
+ */
+XMockMap* xmock_map_create() {
+    // Allocate memory for the map structure
+    XMockMap* map = (XMockMap*)malloc(sizeof(XMockMap));
+
+    if (map == NULL) {
+        return NULL; // Allocation failed
+    } // end if
+
+    // Initialize map properties
+    map->size = 0;
+
+    return map;
+} // end of func
+
+/**
+ * @brief Inserts a key-value pair into the XMockMap.
+ *
+ * @param map A pointer to the XMockMap.
+ * @param key The key to insert.
+ * @param value The corresponding value to insert.
+ */
+void xmock_map_put(XMockMap* map, int key, int value) {
+    if (map->size < XMOCK_MAP_MAX_SIZE) {
+        map->entries[map->size].key = key;
+        map->entries[map->size].value = value;
+        map->size++;
+    } // end if
+} // end of func
+
+/**
+ * @brief Retrieves the value associated with a given key from the XMockMap.
+ *
+ * @param map A pointer to the XMockMap.
+ * @param key The key to look up.
+ *
+ * @return The value associated with the key if found, or a default value of 0 if the key is not found (you can customize this behavior).
+ */
+int xmock_map_get(XMockMap* map, int key) {
+    for (int i = 0; i < map->size; i++) {
+        if (map->entries[i].key == key) {
+            return map->entries[i].value;
+        } // end if
+    } // end for
+    return 0; // Default return value for a key not found (you can customize this behavior)
+} // end of func
+
+/**
+ * @brief Checks if a key exists in the XMockMap.
+ *
+ * @param map A pointer to the XMockMap.
+ * @param key The key to check.
+ *
+ * @return 1 if the key exists in the map, 0 otherwise.
+ */
+int xmock_map_contains(XMockMap* map, int key) {
+    for (int i = 0; i < map->size; i++) {
+        if (map->entries[i].key == key) {
+            return 1; // Key exists
+        } // end if
+    } // end for
+    return 0; // Key not found
+} // end of func
+
+/**
+ * @brief Removes a key-value pair from the XMockMap based on the key.
+ *
+ * @param map A pointer to the XMockMap.
+ * @param key The key to remove.
+ */
+void xmock_map_remove(XMockMap* map, int key) {
+    for (int i = 0; i < map->size; i++) {
+        if (map->entries[i].key == key) {
+            for (int j = i; j < map->size - 1; j++) {
+                map->entries[j] = map->entries[j + 1];
+            } // end for
+            map->size--;
+            break;
+        } // end if
+    } // end for
+} // end of func
+
+/**
+ * @brief Returns the current size (number of key-value pairs) of the XMockMap.
+ *
+ * @param map A pointer to the XMockMap.
+ *
+ * @return The current size of the XMockMap.
+ */
+int xmock_map_size(XMockMap* map) {
+    return map->size;
+} // end of func
+
+/**
+ * @brief Destroys the XMockMap and frees the allocated memory.
+ *
+ * @param map A pointer to the XMockMap to be destroyed.
+ */
+void xmock_map_destroy(XMockMap* map) {
+    if (map != NULL) {
+        free(map);
+    } // end if
+} // end of func
