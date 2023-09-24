b0f22ca96f963dfe14ffd6eb2953c4a18e5298c1
Michael Gene Brockus (Dreamer) <55331536+dreamer-coding-555@users.noreply.github.com>
2023-09-23 20:16:06 -0500

    Update xtest.c

--- src/xtest.c
+++ src/xtest.c
@@ -15,7 +15,7 @@
 #define ANSI_COLOR_RESET   "\x1b[0m"
 
 // Extra
-const char *XTEST_VERSION = "0.3.3";
+const char *XTEST_VERSION = "0.4.0";
 
 // Static control panel for assert/expect and marks
 static bool XEXPECT_PASS_SCAN = true;
