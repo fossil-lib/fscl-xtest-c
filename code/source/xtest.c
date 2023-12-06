72039604d139f1b0be191981c0c9ecf398c50e9d
Michael Gene Brockus <michaelbrockus@gmail.com>
2023-12-05 21:57:41 -0600

    version number to 0.5.0

--- code/source/xtest.c
+++ code/source/xtest.c
@@ -62,7 +62,7 @@ XParser xcli;
 static char *CONFIG_FILENAME[555];
 
 // Extra
-const char *XTEST_VERSION = "0.4.2";
+const char *XTEST_VERSION = "0.5.0";
 
 // Static control panel for assert/expect and marks
 static bool XEXPECT_PASS_SCAN = true;
