31ab1c2f034ffb2f680a232299e04f14e93c1250
Michael Gene Brockus (Dreamer) <55331536+dreamer-coding-555@users.noreply.github.com>
2024-03-10 13:49:07 -0500

    Update xassume.h

--- code/include/fossil/xassume.h
+++ code/include/fossil/xassume.h
@@ -28,21 +28,38 @@ typedef struct cdna cdna;
 typedef enum {
     CDNA_ASSERT_SUCCESS = 0,       // Assertion successful.
     CDNA_ASSERT_FAILURE = -1,      // Assertion failed.
-    CDNA_ASSERT_INVALID_OPERATION  // Invalid assertion operation.
+    CDNA_ASSERT_INVALID_OPERATION, // Invalid assertion operation.
+    CDNA_ASSERT_ERROR_BIT_HIGH,
+    CDNA_ASSERT_ERROR_BIT_NOT_HIGH,
+    CDNA_ASSERT_ERROR_BIT_LOW,
+    CDNA_ASSERT_ERROR_BIT_NOT_LOW,
+    CDNA_ASSERT_ERROR_OUT_OF_RANGE
 } cdna_assert_error;
 
 // Enumerated types for assertion operations.
 typedef enum {
-    CDNA_OPT_EQUAL,
+    CDNA_OPT_ITS_EQUAL,
     CDNA_OPT_NOT_EQUAL,
-    CDNA_OPT_LESS_THAN,
-    CDNA_OPT_LESS_THAN_OR_EQUAL,
-    CDNA_OPT_GREATER_THAN,
-    CDNA_OPT_GREATER_THAN_OR_EQUAL
+    CDNA_OPT_ITS_LESS,
+    CDNA_OPT_NOT_LESS,
+    CDNA_OPT_ITS_LESS_OR_EQUAL,
+    CDNA_OPT_NOT_LESS_OR_EQUAL,
+    CDNA_OPT_ITS_MORE,
+    CDNA_OPT_NOT_MORE,
+    CDNA_OPT_ITS_MORE_OR_EQUAL,
+    CDNA_OPT_NOT_MORE_OR_EQUAL,
+    CDNA_OPT_ITS_BIT_HIGH,
+    CDNA_OPT_NOT_BIT_HIGH,
+    CDNA_OPT_ITS_BIT_LOW,
+    CDNA_OPT_NOT_BIT_LOW,
+    CDNA_OPT_NOT_WITHIN,
+    CDNA_OPT_ITS_WITHIN
 } cdna_opt;
 
 // Enumerated types for representing various data types.
 typedef enum {
+    CDNA_FILE_TYPE,          // FILE IO type.
+    CDNA_SIZE_TYPE,          // Size type.
     CDNA_INT_TYPE,           // Integer type.
     CDNA_INT8_TYPE,          // 8-bit integer type.
     CDNA_INT16_TYPE,         // 16-bit integer type.
@@ -80,6 +97,8 @@ typedef enum {
 
 // Union to hold data of different types.
 typedef union {
+    size_t size_type;          // Size type.
+    FILE *file_type;           // FILE IO type.
     int int_type;              // Integer type.
     int8_t int8_type;          // 8-bit integer type.
     int16_t int16_type;        // 16-bit integer type.
@@ -126,20 +145,103 @@ struct cdna {
     cdna_data data;  // The data stored.
 };
 
-// Helper macro to create a cdna_data structure with a specified type and value
-#define CDNA_CAST(type, value) ((cdna_data){ .type = type, .value = value })
+// Helper function to assign values for different types
+#define CDNA_ASSIGN(data_type, value) ((cdna *)(&(cdna){ .type = data_type, .data = (cdna_data *)(value) }))
 
-// Utility function to simplify assignment assumptions and return a pointer to a static cdna variable
-cdna* assign_gene(cdna_type type, cdna_data value);
+// Helper function to assign file io values
+#define xtest_as_file(value) CDNA_ASSIGN(CDNA_FILE_TYPE, &(cdna_data){ .file_type = value })
+
+// Helper function to assign size type values
+#define xtest_as_size(value) CDNA_ASSIGN(CDNA_SIZE_TYPE, &(cdna_data){ .size_type = value })
+
+// Helper function to assign integer values
+#define xtest_as_int(value) CDNA_ASSIGN(CDNA_INT_TYPE, &(cdna_data){ .int_type = value })
+
+// Helper function to assign 8-bit integer values
+#define xtest_as_int8(value) CDNA_ASSIGN(CDNA_INT8_TYPE, &(cdna_data){ .int8_type = value })
+
+// Helper function to assign 16-bit integer values
+#define xtest_as_int16(value) CDNA_ASSIGN(CDNA_INT16_TYPE, &(cdna_data){ .int16_type = value })
+
+// Helper function to assign 32-bit integer values
+#define xtest_as_int32(value) CDNA_ASSIGN(CDNA_INT32_TYPE, &(cdna_data){ .int32_type = value })
+
+// Helper function to assign 64-bit integer values
+#define xtest_as_int64(value) CDNA_ASSIGN(CDNA_INT64_TYPE, &(cdna_data){ .int64_type = value })
+
+// Helper function to assign unsigned integer values
+#define xtest_as_uint(value) CDNA_ASSIGN(CDNA_UINT_TYPE, &(cdna_data){ .uint_type = value })
+
+// Helper function to assign 8-bit unsigned integer values
+#define xtest_as_uint8(value) CDNA_ASSIGN(CDNA_UINT8_TYPE, &(cdna_data){ .uint8_type = value })
+
+// Helper function to assign 16-bit unsigned integer values
+#define xtest_as_uint16(value) CDNA_ASSIGN(CDNA_UINT16_TYPE, &(cdna_data){ .uint16_type = value })
+
+// Helper function to assign 32-bit unsigned integer values
+#define xtest_as_uint32(value) CDNA_ASSIGN(CDNA_UINT32_TYPE, &(cdna_data){ .uint32_type = value })
+
+// Helper function to assign 64-bit unsigned integer values
+#define xtest_as_uint64(value) CDNA_ASSIGN(CDNA_UINT64_TYPE, &(cdna_data){ .uint64_type = value })
+
+// Helper function to assign octal values
+#define xtest_as_octal(value) CDNA_ASSIGN(CDNA_OCTAL64_TYPE, &(cdna_data){ .octal64_type = value })
+
+// Helper function to assign bitwise values
+#define xtest_as_bitwise(value) CDNA_ASSIGN(CDNA_BITWISE64_TYPE, &(cdna_data){ .bitwise64_type = value })
+
+// Helper function to assign hexadecimal values
+#define xtest_as_hex(value) CDNA_ASSIGN(CDNA_HEX64_TYPE, &(cdna_data){ .hex64_type = value })
+
+// Helper function to assign float values
+#define xtest_as_float(value) CDNA_ASSIGN(CDNA_FLOAT_TYPE, &(cdna_data){ .float_type = value })
+
+// Helper function to assign double values
+#define xtest_as_double(value) CDNA_ASSIGN(CDNA_DOUBLE_TYPE, &(cdna_data){ .double_type = value })
+
+// Helper function to assign string values
+#define xtest_as_string(value) CDNA_ASSIGN(CDNA_STRING_TYPE, &(cdna_data){ .string_type = value })
+
+// Helper function to assign char values
+#define xtest_as_char(value) CDNA_ASSIGN(CDNA_CHAR_TYPE, &(cdna_data){ .char_type = value })
+
+// Helper function to assign boolean values
+#define xtest_as_bool(value) CDNA_ASSIGN(CDNA_BOOLEAN_TYPE, &(cdna_data){ .boolean_type = value })
+
+// Helper function to assign array values
+#define xtest_as_array(elements, size) CDNA_ASSIGN(CDNA_ARRAY_TYPE, &(cdna_data){ .array_type = { .elements = elements, .size = size } })
+
+// Helper function to assign map values
+#define xtest_as_map(key, value, size) CDNA_ASSIGN(CDNA_MAP_TYPE, &(cdna_data){ .map_type = { .key = key, .value = value, .size = size } })
+
+// Helper function to assign quantum bit values
+#define xtest_as_qbit(value) CDNA_ASSIGN(CDNA_QBIT_TYPE, &(cdna_data){ .qbit_type = value })
+
+// Helper function to assign nullptr values
+#define xtest_as_ptr(value) CDNA_ASSIGN(CDNA_NULLPTR_TYPE, &(cdna_data){ .nullptr_type = value })
 
 // Assertion operations
 cdna_assert_error assume(cdna_opt op, const cdna* left, const cdna* right);
+cdna_assert_error assume_not_cnullptr(const cdna* left);
+cdna_assert_error assume_its_cnullptr(const cdna* left);
+cdna_assert_error assume_not_empty(const cdna* left);
+cdna_assert_error assume_its_empty(const cdna* left);
 cdna_assert_error assume_not_equal(const cdna* left, const cdna* right);
 cdna_assert_error assume_its_equal(const cdna* left, const cdna* right);
 cdna_assert_error assume_its_less(const cdna* left, const cdna* right);
 cdna_assert_error assume_its_more(const cdna* left, const cdna* right);
-cdna_assert_error assume_less_or_equal(const cdna* left, const cdna* right);
-cdna_assert_error assume_more_or_equal(const cdna* left, const cdna* right);
+cdna_assert_error assume_not_less(const cdna* left, const cdna* right);
+cdna_assert_error assume_not_more(const cdna* left, const cdna* right);
+cdna_assert_error assume_not_less_or_equal(const cdna* left, const cdna* right);
+cdna_assert_error assume_not_more_or_equal(const cdna* left, const cdna* right);
+cdna_assert_error assume_its_less_or_equal(const cdna* left, const cdna* right);
+cdna_assert_error assume_its_more_or_equal(const cdna* left, const cdna* right);
+cdna_assert_error assume_its_bit_high(const cdna* value, unsigned bit_position);
+cdna_assert_error assume_not_bit_high(const cdna* value, unsigned bit_position);
+cdna_assert_error assume_its_bit_low(const cdna* value, unsigned bit_position);
+cdna_assert_error assume_not_bit_low(const cdna* value, unsigned bit_position);
+cdna_assert_error assume_its_within(const cdna* value, const cdna* lower_bound, const cdna* upper_bound);
+cdna_assert_error assume_not_within(const cdna* value, const cdna* lower_bound, const cdna* upper_bound);
 
 #ifdef __cplusplus
 }
